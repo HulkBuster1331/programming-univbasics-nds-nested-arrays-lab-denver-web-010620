d6883bb7b92154dea94e9d6e6f3c16f074ef58d1
Dillon Franklin <drfranklin1331@gmail.com>
2019-12-19 20:26:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,29 +18,26 @@ CONVENTIONAL_PRODUCE = [
 
 def assembled_matrix
  
- fruits_n_stuff = {}
-  fruits_n_stuff =[CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
- fruits_n_stuff
-end
+   fruits_n_stuff = {}
+     fruits_n_stuff =[CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+    fruits_n_stuff
+  end
 
 def sorted_matrix
   fruits_n_stuff = {}
    fruits_n_stuff =[CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
 
- fruits_n_stuff
-end
+  fruits_n_stuff
+ end
 
 def matrix_lookup(matrix, row, column)
-
- matrix[row][column]
-
- 
- 
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
-end
+    matrix[row][column]
+  end
 
 def matrix_update(matrix, row, column, new_value)
+ 
+ matrix[row][column]= "new_value"
+ 
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
